ea31891ed3afe85b480584281471c4716d45d2d9
Edgar Castro <edgarcastro@users.noreply.github.com>
2025-09-17 17:21:43 -0500

    Move to next

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as SignUpForm } from './SignUpForm';
-export { default as Copyright } from './Copyright';
-export { default as SignInForm } from './SignInForm';
--- src/pages/LoadingPage/LoadingPage.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const LoadingPage: React.FC = () => <span>Loading...</span>;
